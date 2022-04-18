5ffb30bb2591fcf274c70bf24fe04a8c0dbba464
Suyannesara <suyannesara17@gmail.com>
2022-04-18 20:51:57 -0300

    Started JS part

---
+++ script.js
@@ -0,0 +1,17 @@
+// 1 - ler os dados
+function readData(){
+  let produto = {}
+
+  produto.nomeProduto = document.getElementById("nameProduto").name;
+
+  alert(JSON.stringify(produto))
+  alert("aaaaaa")
+  
+}
+
+
+
+/*produto.preco = document.getElementById("precoProduto").value
+produto.qtdProduto = document.getElementById("qtdProduto").value
+
+return produto*/
